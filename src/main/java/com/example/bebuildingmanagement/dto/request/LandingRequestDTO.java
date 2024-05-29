d3b7f3df9d80733fa0df02a1c54e929f8d3a57f1
lechithien0901 <117095269+lechithien0901@users.noreply.github.com>
2024-05-29 19:02:15 +0700

    up

---
+++ src/main/java/com/example/bebuildingmanagement/dto/request/LandingRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.bebuildingmanagement.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LandingRequestDTO {
+    Long id;
+    String code;
+    String type;
+    double area;
+    String status;
+    String description;
+    double feePerMonth;
+    double feeManager;
+    FloorRequestDTO floor;
+    String firebaseUrl;
+}
---
+++ src/main/java/com/example/bebuildingmanagement/dto/response/LandingResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.bebuildingmanagement.dto.response;
+
+import com.example.bebuildingmanagement.dto.request.FloorRequestDTO;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LandingResponseDTO {
+    Long id;
+    String code;
+    String type;
+    double area;
+    String status;
+    String description;
+    double feePerMonth;
+    double feeManager;
+    FloorRequestDTO floor;
+    String firebaseUrl;
+}
