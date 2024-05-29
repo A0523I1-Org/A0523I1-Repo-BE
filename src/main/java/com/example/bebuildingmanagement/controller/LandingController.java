95530f6d5e984d8d836a9afaadfad19c03cc405b
lechithien0901 <117095269+lechithien0901@users.noreply.github.com>
2024-05-29 19:02:15 +0700

    up

--- src/main/java/com/example/bebuildingmanagement/controller/LandingController.java
+++ src/main/java/com/example/bebuildingmanagement/controller/LandingController.java
@@ -1,18 +1,19 @@
 package com.example.bebuildingmanagement.controller;
 
+
+import com.example.bebuildingmanagement.dto.request.ApiResponseDTO;
+import com.example.bebuildingmanagement.dto.request.LandingRequestDTO;
 import com.example.bebuildingmanagement.dto.response.FloorResponseDTO;
 import com.example.bebuildingmanagement.dto.response.LandingResponseDTO;
 import com.example.bebuildingmanagement.service.interfaces.IFloorService;
 import com.example.bebuildingmanagement.service.interfaces.ILandingService;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,22 +25,26 @@ public class LandingController {
     ILandingService iLandingService;
     IFloorService iFloorService;
 
+
     @GetMapping
     List<LandingResponseDTO> getListAllLanding() {
         return iLandingService.showListLanding();
     }
 
-//    @PostMapping
-//    ApiResponseDTO<LandingResponseDTO> createLanding(@RequestBody @Valid LandingRequestDTO landingRequestDTO) {
-//        ApiResponseDTO<LandingResponseDTO> apiResponseDTO = new ApiResponseDTO<>();
-//        apiResponseDTO.setResult(iLandingService.createLanding(landingRequestDTO));
-//        return apiResponseDTO;
-//
-//    }
+    @PostMapping
+    ApiResponseDTO<LandingResponseDTO> createLanding(@RequestBody @Valid LandingRequestDTO landingRequestDTO) {
+        ApiResponseDTO<LandingResponseDTO> apiResponseDTO = new ApiResponseDTO<>();
+        apiResponseDTO.setResult(iLandingService.createAndUpdateLanding(landingRequestDTO));
+        return apiResponseDTO;
+
+    }
 
     @GetMapping("/listFloor")
     ResponseEntity<List<FloorResponseDTO>> getListAllFloor() {
         List<FloorResponseDTO> floorResponseDTOList = iFloorService.getFloor();
         return new ResponseEntity<>(floorResponseDTOList, HttpStatus.OK);
     }
+
+
+
 }
