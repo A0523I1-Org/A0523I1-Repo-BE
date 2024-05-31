a3f75810050b1b0ff50b968d77e733a81c4dd9c6
Tran Le Quang Trieu <trieutran025@gmail.com>
2024-05-31 20:15:57 +0700

    trieuTLQ

--- src/main/java/com/example/bebuildingmanagement/service/interfaces/ICustomerService.java
+++ src/main/java/com/example/bebuildingmanagement/service/interfaces/ICustomerService.java
@@ -2,13 +2,14 @@ package com.example.bebuildingmanagement.service.interfaces;
 
 import com.example.bebuildingmanagement.dto.request.CustomerRequestDTO;
 import com.example.bebuildingmanagement.dto.response.CustomerResponseDTO;
+import com.example.bebuildingmanagement.entity.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Date;
 
 
-@Service
+
 public interface ICustomerService {
     Page<CustomerResponseDTO> getAllCustomer(Pageable pageable);
 
