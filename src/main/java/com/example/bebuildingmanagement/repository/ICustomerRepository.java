7dfb48c48318ab4820df8eb7ae8c8042342243a1
Thong <you@example.com>
2024-05-31 20:28:14 +0700

    customer

--- src/main/java/com/example/bebuildingmanagement/repository/ICustomerRepository.java
+++ src/main/java/com/example/bebuildingmanagement/repository/ICustomerRepository.java
@@ -21,7 +21,7 @@ import java.util.Date;
 
 @Repository
 public interface ICustomerRepository extends JpaRepository<Customer, Long> {
-   @Query(value = "select id,dob,gender, name, phone,id_card, email,address,website,company_name,is_deleted from customer;", nativeQuery = true)
+   @Query(value = "select id,dob,gender, name, phone,id_card, email,address,website,company_name,is_deleted from customer where is_deleted = 0;", nativeQuery = true)
     Page<Customer> getAllCustomer(Pageable pageable);
     @Modifying
     @Transactional
