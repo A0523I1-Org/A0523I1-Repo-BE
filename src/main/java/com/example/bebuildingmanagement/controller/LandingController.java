17e94bd8bdfb04ba98307cad8e1961aac88cb0cc
Phan Phùng <phanvanphung6102001@gmail.com>
2024-06-07 17:50:02 +0700

    Update LandingController.java

--- src/main/java/com/example/bebuildingmanagement/controller/LandingController.java
+++ src/main/java/com/example/bebuildingmanagement/controller/LandingController.java
@@ -22,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/landing")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class LandingController {
     ILandingService iLandingService;
