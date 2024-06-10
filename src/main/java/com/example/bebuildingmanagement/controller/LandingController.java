1dfd0b006d2a756f6d9f904e63f5b25704c1fa8e
DienTon <thatdien295@gmail.com>
2024-06-10 19:25:25 +0700

    up

--- src/main/java/com/example/bebuildingmanagement/controller/LandingController.java
+++ src/main/java/com/example/bebuildingmanagement/controller/LandingController.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/landing")
-@CrossOrigin("*")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @CrossOrigin("*")
