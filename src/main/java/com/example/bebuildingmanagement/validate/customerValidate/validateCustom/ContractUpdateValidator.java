8e94e39645c783561529a6c3fbef51fc25b32116
lechithien0901 <117095269+lechithien0901@users.noreply.github.com>
2024-06-27 23:33:32 +0700

    up

--- src/main/java/com/example/bebuildingmanagement/validate/customerValidate/validateCustom/ContractUpdateValidator.java
+++ src/main/java/com/example/bebuildingmanagement/validate/customerValidate/validateCustom/ContractUpdateValidator.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 public class ContractUpdateValidator implements Validator {
     @Autowired
     private IContractRepository iContractRepository;
-
+  
     @Override
     public boolean supports(Class<?> clazz) {
         return false;
